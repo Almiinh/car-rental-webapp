0bf7222ed2e015e8103f29c8b07e700b6b7bfb2f
Minh-Hoang HUYNH <minh-hoang.huynh@outlook.fr>
2023-06-08 13:31:13 +0200

    Minor changes

--- migrations/Version20230608111108.php
+++ migrations/Version20230608111108.php
@@ -23,7 +23,6 @@ final class Version20230608111108 extends AbstractMigration
         $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) DEFAULT NULL, code_postal INT DEFAULT NULL, rue VARCHAR(255) DEFAULT NULL, ville VARCHAR(255) DEFAULT NULL, tel VARCHAR(32) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE contrat (id INT AUTO_INCREMENT NOT NULL, client_id INT NOT NULL, voiture_id INT DEFAULT NULL, type VARCHAR(255) DEFAULT NULL, date_depart DATE DEFAULT NULL, date_retour DATE DEFAULT NULL, INDEX IDX_6034999319EB6921 (client_id), UNIQUE INDEX UNIQ_60349993181A8BA (voiture_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE facture (id INT AUTO_INCREMENT NOT NULL, client_id INT NOT NULL, date DATE DEFAULT NULL, compteur_km INT DEFAULT NULL, montant_ht INT DEFAULT NULL, montant_a_payer INT DEFAULT NULL, INDEX IDX_FE86641019EB6921 (client_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE panda (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE parking (id INT AUTO_INCREMENT NOT NULL, capacite INT DEFAULT NULL, rue VARCHAR(255) DEFAULT NULL, arrondissement VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE voiture (id INT AUTO_INCREMENT NOT NULL, client_id INT DEFAULT NULL, parking_id INT DEFAULT NULL, immatriculation VARCHAR(255) NOT NULL, marque VARCHAR(255) DEFAULT NULL, modele VARCHAR(255) DEFAULT NULL, carburant VARCHAR(255) DEFAULT NULL, capacite INT DEFAULT NULL, INDEX IDX_E9E2810F19EB6921 (client_id), INDEX IDX_E9E2810FF17B2DD (parking_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -45,7 +44,6 @@ final class Version20230608111108 extends AbstractMigration
         $this->addSql('DROP TABLE client');
         $this->addSql('DROP TABLE contrat');
         $this->addSql('DROP TABLE facture');
-        $this->addSql('DROP TABLE panda');
         $this->addSql('DROP TABLE parking');
         $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE voiture');
